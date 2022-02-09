f3c8daa1b6918ea5ad151ca59afa0a15ae172b28
Mohamad Suhaib <99238465+IT21172496@users.noreply.github.com>
2022-02-09 04:18:40 +0000

    Tutorial1 Exercise 1

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,19 @@
 #include <stdio.h>
 
 int main() {
-  
+  int marks1,marks2;
+  float avg,sum=0; //avg=average
+
+  printf("Enter the first number:");
+  scanf("%d",&marks1);
+
+  printf("Enter the second number:");
+  scanf("%d",&marks2);
+
+  avg=(marks1+marks2)/2;
+
+  printf("avg is %.f",avg);
   return 0;
 }
+
 
